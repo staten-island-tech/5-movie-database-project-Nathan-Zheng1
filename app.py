c01a6aad40c3d6dfb10f538dfe7b0ce25d7204db
Nathan-Zheng1 <nathanz32@nycstudents.net>
2025-10-24 06:49:16 -0700

    Update app.py

--- app.py
+++ app.py
@@ -1,6 +1,27 @@
+
 import json
-## Open the JSON file of movie data
 movies = open("./movies.json", encoding="utf8")
-## create variable "data" that represents the enitre movie list
 data = json.load(movies)
+for index, item in enumerate(data):
+    print(index, ":" , item["title"])
+user_input = input("Here is our list of movies! Please enter one of the following variables and we will search for it: genres, title, or year")
+if user_input == "genres":
+    user_input = input("What genre are you looking for?")
+    if user_input == "comedy":
+        for index, item in enumerate(data):
+            if item["genres"] == "Comedy":
+                print(f"All the movies listed fall under the genre of comedy: {item["title"]}")
+                user_input = input("Are any of these movies the one you are looking for? If so, please type its title word for word.")
+                for index, item in enumerate(data):
+                    if item["title"] == user_input:
+                        print(f"{item["title"]} was made in {item["year"]}, and falls under the {item["genres"]} genre")
+if user_input == "year":
+    user_input = input("Please enter a year you would like us to search for:")
+    for index, item in enumerate(movies):
+        if item["year"] == user_input:
+            print(f"These movies were created on {user_input}: {item["title"]}")
+        if item["year"] < user_input:
+            print(f"These movies were created before {user_input}: {item["title"]}")
+        if item["year"] > user_input:
+            print(f"These movies were produced after {user_input}: {item["title"]}")
 
