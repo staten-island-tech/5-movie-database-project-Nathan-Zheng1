cdb518fbc555497b965b9f90a51b54ea4bae0acf
Nathan-Zheng1 <nathanz32@nycstudents.net>
2025-11-05 06:53:17 -0800

    Update app.py

--- app.py
+++ app.py
@@ -7,9 +7,16 @@ def database(y):
     name = item["title"]
     date = item["year"]
     genre = item["genres"]
+    description = item["extract"]
     user_input = input("Here is our list of movies! Please enter one of the following variables and we will search for it: genres, title, or year")
+    if user_input is not "genres":
+        print("Invalid Input")
+        return
     if user_input == "genres":
         user_input = input("What genre are you looking for?")
+        if genre is not user_input:
+            print("Invalid Input")
+            return
         if genre == [user_input]:
             print(item["title"])
     if user_input == "year":
@@ -23,6 +30,7 @@ def database(y):
             print(f"These movies were produced before {x}: {name}")
     if user_input == "title":
         user_input = input("Please type the specific title of a movie")
-        if user_input == name:
-            print(f"The movie '{name}' was produced on {date}, and falls into the genre(s) of {genre}")
+        for index, item in enumerate(data):
+            if user_input == name:
+                print(f"The movie '{name}' was produced on {date}, and falls into the genre(s) of {genre}. {description}")
 database(0)
