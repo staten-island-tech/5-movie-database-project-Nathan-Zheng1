aa42f25fd2cb190567a52c337195f01f9fc529a8
Nathan-Zheng1 <nathanz32@nycstudents.net>
2025-11-17 06:49:31 -0800

    Update app.py

--- app.py
+++ app.py
@@ -4,27 +4,25 @@ def database(y):
     data = json.load(movies)
     for index, item in enumerate(data):
         print(index, ":" , item["title"])
-    name = item["title"]
-    date = item["year"]
-    genre = item["genres"]
-    description = item["extract"]
     user_input = input("Here is our list of movies! Please enter one of the following variables and we will search for it: genres, title, or year")
     if user_input == "genres":
         user_input = input("What genre are you looking for?")
-        if genre == [user_input]:
-            print(item["title"])
+        for item in data:
+            if item["genres"] == [user_input]:
+                print(item["title"])
     if user_input == "year":
         user_input = input("What year are you looking for?")
         x = int(user_input)
-        if date == x:
-            print(f"These movies were produced on {x}: {name}")
-        elif date > x:
-            print(f"These movies were produced after {x}: {name}")
-        elif date < x:
-            print(f"These movies were produced before {x}: {name}")
+        for index, item in enumerate(data):
+            if item["year"] == x:
+                print(f"These movies were produced on {x}: {item["title"]}")
+            elif item["year"] > x:
+                print(f"These movies were produced after {x}: {item["title"]}")
+            elif item["year"] < x:
+                print(f"These movies were produced before {x}: {item["title"]}")
     if user_input == "title":
         user_input = input("Please type the specific title of a movie")
         for index, item in enumerate(data):
-            if user_input == name:
-                print(f"The movie '{name}' was produced on {date}, and falls into the genre(s) of {genre}. {description}")
+            if user_input == item["title"]:
+                print(f"The movie '{item["title"]}' was produced on {item["year"]}, and falls into the genre(s) of {item["genres"]}.")
 database(0)
